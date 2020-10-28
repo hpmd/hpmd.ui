d8cc758633ffe629c5ac4aca6142f2c935ab4359
revan63 <alexandrfileev@gmail.com>
2020-10-28 14:31:17 +0300

    fix

--- src/components/HmRangeSlider.vue
+++ src/components/HmRangeSlider.vue
@@ -4,10 +4,6 @@ import VueSlider from 'vue-slider-component';
 export default {
     extends: VueSlider,
     props: {
-        height: {
-            type: Number,
-            default: 2
-        },
         dotSize: {
             type: Number,
             default: 20
@@ -15,6 +11,32 @@ export default {
         tooltipPlacement: {
             type: String,
             default: 'bottom'
+        }
+    },
+    computed: {
+        containerStyles() {
+            return {};
+        },
+        dotBaseStyle() {
+            let dotPos;
+
+            if (this.isHorizontal) {
+                dotPos = {
+                    transform: `translate(${this.isReverse ? '50%' : '-50%'}, -50%)`,
+                    WebkitTransform: `translate(${this.isReverse ? '50%' : '-50%'}, -50%)`,
+                    top: '50%',
+                    [this.direction === 'ltr' ? 'left' : 'right']: '0',
+                };
+            } else {
+                dotPos = {
+                    transform: `translate(-50%, ${this.isReverse ? '50%' : '-50%'})`,
+                    WebkitTransform: `translate(-50%, ${this.isReverse ? '50%' : '-50%'})`,
+                    left: '50%',
+                    [this.direction === 'btt' ? 'bottom' : 'top']: '0',
+                };
+            }
+
+            return dotPos;
         }
     }
 }; // https://nightcatsama.github.io/vue-slider-component

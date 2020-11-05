8f78f9b66516a3d0846c9cdb35315a809071ee50
revan63 <alexandrfileev@gmail.com>
2020-11-05 18:35:29 +0300

    added texttip component

---
+++ src/components/HmTextTip.vue
@@ -0,0 +1,39 @@
+<script>
+import HmTooltip from '@/components/HmTooltip';
+
+export default {
+    name: 'HmTextTip',
+    props: {
+        tip: {
+            type: String,
+            default: ''
+        },
+        triggers: {
+            type: String,
+            default: 'hover'
+        }
+    },
+    render() {
+        const rndId = Math.round(Math.random() * 9999);
+
+        return (
+            <span>
+                <span
+                    id={`hm-texttip-${rndId}`}
+                    class="hm-texttip"
+                    title={this.tip}>
+                    {this.$slots.default}
+                </span>
+                <HmTooltip
+                    target={`hm-texttip-${rndId}`}
+                    triggers={this.triggers}>
+                    {this.tip}
+                </HmTooltip>
+            </span>
+        );
+    },
+    components: {
+        HmTooltip
+    }
+};
+</script>
